fa2e6329c963978e11d947e8fc270bfd2c5b3a48
Luis Josafat Heredia Contreras <luisyosafat@gmail.com>
2017-04-17 10:19:01 -0500

    update version

--- App/Drive/Database/Seeds/ApplicationSeeder.php
+++ App/Drive/Database/Seeds/ApplicationSeeder.php
@@ -18,7 +18,7 @@ class ApplicationSeeder extends InstallSeeder
             'description'=>'Application Drive',
             'nameSpace'=>'Melisa.drive',
             'typeSecurity'=>'art',
-            'version'=>'1.2.0'
+            'version'=>'1.2.1'
         ]);
         
     }
